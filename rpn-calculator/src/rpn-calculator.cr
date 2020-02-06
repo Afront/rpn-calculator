b7d185ac7d28f99cf6cf7bae48118b834673c8d6
Afront <3943720+Afront@users.noreply.github.com>
2020-02-07 00:29:32 +0900

    Add support for negative numbers (with an exception)
    Only works if the negative sign is applied once to a number. It does not support expressions with more than 2 consecutive hypen-minus symbols (i.e. 1 - --2).  I might add this feature when refactoring it

--- rpn-calculator/src/rpn-calculator.cr
+++ rpn-calculator/src/rpn-calculator.cr
@@ -20,6 +20,9 @@ module RPNCalculator
     stack.pop # or stack[0]
   end
 
+  def compare_precedence?(token, top)
+  end
+
   def do_shunting_yard(input : String)
     output_stack = [] of String
     op_stack = [] of Char
@@ -32,43 +35,63 @@ module RPNCalculator
       '%' => {:precedence => 2, :associativity => :left},
     }
 
-    input.chars.each do |token| # add support for negative numbers
+    is_negative_sign = true
+    input.chars.each_with_index do |token, index| # add support for negative numbers
+      puts "Token: #{token}"
+      puts "Before - during loop\nOutput stack: #{output_stack}, Num stack: #{num_stack}, OP Stack: #{op_stack}"
       next if token.whitespace?
-      if token.to_i? || token == '.'
-        num_stack << token
-      elsif ops_hash.fetch(token, false)
+      next num_stack << token if token.to_i? || token == '.'
+
+      unless num_stack.empty?
         output_stack << num_stack.join
+        is_negative_sign = false
         num_stack.clear
-        unless op_stack.empty?
-          break if op_stack.last == '('
-          top_precedence = ops_hash[op_stack.last][:precedence].to_i
-          tkn_precedence = ops_hash[token][:precedence].to_i
-          tkn_associativity = ops_hash[token][:associativity]
-          while (op_stack.last != '(') ||
-                (top_precedence > tkn_precedence) ||
-                (top_precedence == tkn_precedence && tkn_associativity == :left) &&
-                output_stack << op_stack.pop.to_s
+      end
+
+      if ops_hash.fetch(token, false)
+        if is_negative_sign
+          num_stack.insert(0, '-')
+        else
+          unless op_stack.empty?
+            unless op_stack.last == '('
+              top_precedence = ops_hash[op_stack.last][:precedence].to_i
+              tkn_precedence = ops_hash[token][:precedence].to_i
+              tkn_associativity = ops_hash[token][:associativity]
+              while (op_stack.last != '(') ||
+                    (top_precedence > tkn_precedence) ||
+                    (top_precedence == tkn_precedence && tkn_associativity == :left) &&
+                    output_stack << op_stack.pop.to_s
+              end
+            end
           end
+          p "Hi again"
+          op_stack << token
+          is_negative_sign = true
         end
-        op_stack << token
       elsif token == '('
         op_stack << '('
+        is_negative_sign = true
       elsif token == ')'
         while op_stack.last != '('
           output_stack << op_stack.pop.to_s
         end
         # Add actual error if operator stack is empty
-        raise "Parentheses Error: Missing ( @ column..." if op_stack.empty?
+        raise "Parentheses Error: Missing '(' to match the ')' @ column #{index + 1}!" if op_stack.empty?
         op_stack.pop if op_stack.last == '('
+        is_negative_sign = false
       else
         p token.to_f?
         p ops_hash.fetch(token, false)
         return "Not supported yet #{token}"
       end
+      puts "After - during loop\nOutput stack: #{output_stack}, Num stack: #{num_stack}, OP Stack: #{op_stack}"
     end
     output_stack << num_stack.join unless num_stack.empty?
+
+    puts "After loop\nOutput stack: #{output_stack}, Num stack: #{num_stack}, OP Stack: #{op_stack}"
+
     until op_stack.empty?
-      raise "Parentheses Error: Missing ) @ column..." if op_stack.last == '('
+      raise "Parentheses Error: Missing ')' at the end!" if op_stack.last == '('
       output_stack << op_stack.pop.to_s
     end
 
