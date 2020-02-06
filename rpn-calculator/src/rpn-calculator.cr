8253c7e6366d4f0c175a82b9bcc705978fced383
Afront <3943720+Afront@users.noreply.github.com>
2020-02-06 20:28:16 +0900

    Add old Ruby code to RPNCalculator

--- rpn-calculator/src/rpn-calculator.cr
+++ rpn-calculator/src/rpn-calculator.cr
@@ -2,5 +2,13 @@
 module RPNCalculator
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  def calculate_rpn_automata(input : String)
+    stack = [] of Float64
+    ops = ['+', '-', '/', '*', '%']
+
+    input.split.each do |i|
+      stack << ((ops.includes? i) ? stack.pop.send(i, stack.pop.to_f) : i.to_f)
+    end
+    stack.pop # or stack[0]
+  end
 end
