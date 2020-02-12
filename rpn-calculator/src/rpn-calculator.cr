d42285dd8791706053d81a3cd749355f775a4c15
Afront <3943720+Afront@users.noreply.github.com>
2020-02-12 21:14:17 +0900

    Refactor code in RPNCalculator

--- rpn-calculator/src/rpn-calculator.cr
+++ rpn-calculator/src/rpn-calculator.cr
@@ -46,6 +46,20 @@ module RPNCalculator
     end
   end
 
+  def check_precedence(output_stack, op_stack, token)
+    unless op_stack.last == '('
+      top_precedence = OPS_HASH[op_stack.last][:precedence].as(Int32)
+      tkn_precedence = OPS_HASH[token][:precedence].as(Int32)
+      tkn_associativity = OPS_HASH[token][:associativity].as(Symbol)
+      while !op_stack.empty? && op_stack.last != '(' &&
+            ((top_precedence > tkn_precedence) ||
+            (top_precedence == tkn_precedence && tkn_associativity == :left))
+        output_stack << op_stack.pop.to_s
+      end
+    end
+    {output_stack, op_stack}
+  end
+
   # Converts the given *input* expression into a postfix notation expression
   # ```
   # do_shunting_yard("1+2") # => "1 2 +"
@@ -58,11 +72,13 @@ module RPNCalculator
     prev_token = '\n'
     input.chars.each_with_index do |token, index|
       next if token.whitespace?
+
       if token.to_i? || token == '.'
         op_stack << '*' if prev_token == ')'
         prev_token = token
         next number.numbers << token
       end
+
       unless number.numbers.empty?
         output_stack << number.to_s
         dash_is_negative_sign = false
@@ -73,35 +89,13 @@ module RPNCalculator
         if dash_is_negative_sign
           number.is_negative ^= true
         else
-          unless op_stack.empty?
-            unless op_stack.last == '('
-              top_precedence = OPS_HASH[op_stack.last][:precedence].as(Int32)
-              tkn_precedence = OPS_HASH[token][:precedence].as(Int32)
-              tkn_associativity = OPS_HASH[token][:associativity].as(Symbol)
-              while !(op_stack.empty?) && (op_stack.last != '(') &&
-                    ((top_precedence > tkn_precedence) ||
-                    (top_precedence == tkn_precedence && tkn_associativity == :left))
-                output_stack << op_stack.pop.to_s
-              end
-            end
-          end
+          output_stack, op_stack = check_precedence(output_stack, op_stack, token) unless op_stack.empty?
           op_stack << token
           dash_is_negative_sign = true
         end
       elsif token == '('
         if prev_token == ')' || prev_token.to_i?
-          unless op_stack.empty?
-            unless op_stack.last == '('
-              top_precedence = OPS_HASH[op_stack.last][:precedence].as(Int32)
-              tkn_precedence = OPS_HASH['*'][:precedence].as(Int32)
-              tkn_associativity = OPS_HASH['*'][:associativity].as(Symbol)
-              while !(op_stack.empty?) && (op_stack.last != '(') &&
-                    ((top_precedence > tkn_precedence) ||
-                    (top_precedence == tkn_precedence && tkn_associativity == :left))
-                output_stack << op_stack.pop.to_s
-              end
-            end
-          end
+          output_stack, op_stack = check_precedence(output_stack, op_stack, '*') unless op_stack.empty?
           op_stack << '*'
         end
         op_stack << '('
@@ -140,7 +134,7 @@ module RPNCalculator
         elsif (['+', '-', '*', '/', '%'].includes? input[-1]) && input.strip.size != 1
           p calculate_rpn(input || "")
         else
-          # p do_shunting_yard(input || "")
+          p do_shunting_yard(input || "")
           p calculate_rpn do_shunting_yard(input || "")
         end
       rescue error_msg : Exception
