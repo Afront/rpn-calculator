b4471a429f4996002ecaf8a7bc12ed3a35364382
Afront <3943720+Afront@users.noreply.github.com>
2020-02-15 19:54:10 +0900

    Refactor code for RPN assignments

--- rpn-calculator/src/rpn-calculator.cr
+++ rpn-calculator/src/rpn-calculator.cr
@@ -6,13 +6,79 @@ require "./parser"
 module RPNCalculator
   VERSION = "0.2.0"
 
-  class Calculator
-    getter var_hash
+  class Token
+    include Parser
+    getter token : String | Float64
+    @@var_hash = {} of String => Float64
 
+    def initialize(@token)
+      raise "Token is invalid!" if !is_valid?
+    end
+
+    def type : Class
+      @token.class
+    end
+
+    def is_alphanumeric? : Bool
+      string.chars.each { |c| return false unless c.alphanumeric? } || true
+    end
+
+    def is_f? : Bool
+      type == Float64 || token.as(String).to_f? != nil
+    end
+
+    def is_op? : Bool
+      OPS_HASH.fetch(token.to_s.char_at(-1), false) != false
+    end
+
+    def is_s? : Bool
+      type == String
+    end
+
+    def is_valid? : Bool
+      is_f? || is_s? || is_op?
+    end
+
+    def to_f : Float64
+      is_f? ? token.to_f : @@var_hash[token]
+    end
+
+    def to_s : String
+      token.to_s
+    end
+
+    def ==(other : String | Char) : Bool
+      token == other.to_s
+    end
+
+    def ==(other : Token) : Bool
+      token = Token.token
+    end
+
+    def self.var_hash
+      @@var_hash
+    end
+
+    def operate(popped_tokens)
+      Token.new(case popped_tokens.size
+      when 1
+        -3.14
+        # OPS_HASH[token.to_s[0]][:proc].as(Proc(Float64, Float64, Float64)).call(*popped_tokens.as(Tuple(Float64)))
+      when 2
+        OPS_HASH[token.to_s[0]][:proc].as(Proc(Float64, Float64, Float64)).call(*popped_tokens.as(Tuple(Float64, Float64)))
+      when 3
+        -3.14
+        # OPS_HASH[token[0].to_s][:proc].as(Proc(Float64, Float64, Float64)).call(*popped_tokens.as(Tuple(Float64, Float64, Float64)))
+      else
+        -3.14
+      end)
+    end
+  end
+
+  class Calculator
     include Parser
 
     def initialize
-      @var_hash = {} of String => Float64
     end
 
     enum Notation
@@ -22,20 +88,13 @@ module RPNCalculator
       Error
     end
 
-    def convert_to_f(token : String | Float64) : Float64
-      (token.is_a? String) ? @var_hash[token] : token.to_f
-    end
-
-    def is_alphanumeric(string : String) : Bool
-      string.chars.each { |c| return false unless c.alphanumeric? } || true
-    end
-
     def check_notation(expression : String) : Notation
+      exp_token = Token.new(expression)
       exp_array = expression.split.map { |c| c.to_i? }
       functions = ['+', '-', '*', '/', '%', '^', '=']
-      if expression.to_i? || is_alphanumeric(expression) || functions.includes? expression[-1]
+      if exp_token.is_valid?
         Notation::Postfix
-      elsif (exp_array[0] && exp_array[-1].class == Int32) || expression.includes?(')') || exp_array.size == 1
+      elsif (exp_array[0] && exp_array[-1].is_a? Int32) || expression.includes?(')') || exp_array.size == 1
         Notation::Infix
       elsif exp_array[-2..-1].select(nil).empty?
         Notation::Prefix
@@ -47,10 +106,10 @@ module RPNCalculator
       end
     end
 
-    def assign(stack : Array(Float64 | String)) : Array(Float64 | String)
+    def assign(stack : Array(Token)) : Array(Token)
       arity = OPS_HASH['='][:proc].as(Proc).arity.to_i
       value = stack.pop
-      @var_hash[stack.pop.to_s] = value.to_f
+      Token.var_hash[stack.pop.to_s] = value.to_f
       stack << value
       stack
     end
@@ -60,29 +119,27 @@ module RPNCalculator
     # calculate("1 2 +") # => 3
     # ```
     def calculate(input : String) : Float64
-      stack = [] of Float64 | String
+      stack = [] of Token
 
-      input.split.each do |token|
-        op = token.char_at(-1)
-        raise DivisionByZeroError.new("Error: Attempted dividing by zero") if op == '/' && stack.last == 0
-        raise ArgumentError.new("Error: Not enough arguments!") if (is_op = OPS_HASH.fetch(op, false)) && stack.size < 2
-        stack << if is_op
+      input.split.each do |string|
+        token = Token.new(string)
+        raise DivisionByZeroError.new("Error: Attempted dividing by zero") if token == '/' && stack.last == 0
+        raise ArgumentError.new("Error: Not enough arguments!") if token.is_op? && stack.size < 2
+        stack << if token.is_op?
           next assign(stack) if token == "="
-          stack, popped_tokens = token_pop(stack, OPS_HASH[op][:proc].as(Proc).arity.to_i)
-          evaluate_expression(op, popped_tokens)
-        elsif token.to_f?
-          token.to_f
+          stack, popped_tokens = token_pop(stack, OPS_HASH[string[-1]][:proc].as(Proc).arity.to_i)
+          token.operate(popped_tokens)
         else
           token
         end
       end
       raise ArgumentError.new("Error: Missing operator!") if stack.size > 1
-      convert_to_f(stack.pop) # or convert_to_f(stack[0])
+      stack.pop.to_f # or stack[0].to_f
     end
 
-    def token_pop(stack : Array(Float64 | String), arity : Int32) : Tuple(Array(Float64 | String), Tuple(Float64) | Tuple(Float64, Float64) | Tuple(Float64, Float64, Float64))
+    def token_pop(stack : Array(Token), arity : Int32) : Tuple(Array(Token), Tuple(Float64) | Tuple(Float64, Float64) | Tuple(Float64, Float64, Float64))
       popped_tokens = [] of Float64
-      arity.times { popped_tokens << convert_to_f(stack.pop) }
+      arity.times { popped_tokens << stack.pop.to_f }
       # Convert popped_tokens to numbers only -> var to numbers method
       arg_tuple = case arity
                   when 1
@@ -96,21 +153,6 @@ module RPNCalculator
                   end
 
       {stack, arg_tuple}
-    end
-
-    def evaluate_expression(op, popped_tokens) : Float64
-      case popped_tokens.size
-      when 1
-        -3.14
-        # OPS_HASH[op][:proc].as(Proc(Float64, Float64, Float64)).call(*popped_tokens.as(Tuple(Float64)))
-      when 2
-        OPS_HASH[op][:proc].as(Proc(Float64, Float64, Float64)).call(*popped_tokens.as(Tuple(Float64, Float64)))
-      when 3
-        -3.14
-        # OPS_HASH[op][:proc].as(Proc(Float64, Float64, Float64)).call(*popped_tokens.as(Tuple(Float64, Float64, Float64)))
-      else
-        -3.14
-      end
     end
 
     # Is an interactive prompt that allows users to get the results of any legal expresssion
