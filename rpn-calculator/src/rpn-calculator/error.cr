fe16e1f369033b29f3b4fbebf737c086e62adc16
Afront <3943720+Afront@users.noreply.github.com>
2020-02-20 22:52:29 +0900

    Add docs for Error module

--- rpn-calculator/src/rpn-calculator/error.cr
+++ rpn-calculator/src/rpn-calculator/error.cr
@@ -1,10 +1,13 @@
+# Contains all the custom exceptions/errors for the program
 module Error
+  # Occurs when the user wants to find the factorial of a negative integer
   class FactorialOfNegativeIntegersError < Exception
     def initialize(message = "Error: Cannot find the factorial of a negative integer")
       super message
     end
   end
 
+  # A convenient helper to easily raise an error
   def raise(type : String)
   end
 end
